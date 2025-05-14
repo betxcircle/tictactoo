8ae01d93860f0134f187031a57656a12b30f36c0
betxcircle <150256421+betxcircle@users.noreply.github.com>
2025-05-14 18:26:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -290,9 +290,10 @@ const startTurnTimer = (roomId) => {
 
     startTurnTimer(roomId); // Restart timer
       
-      const winnerSymbol = checkWin(room.board);
+        const winResult = checkWin(room.board);
 
-      if (winnerSymbol) {
+      if (winResult) {
+          const { winnerSymbol, winningLine } = winResult;
         clearTimeout(room.turnTimeout); // **Stop turn timer if someone wins**
         
         const winnerPlayer = room.players.find(player => player.symbol === winnerSymbol);
@@ -326,6 +327,12 @@ console.log('Winner balance updated successfully');
           //   loserUserId, 
           //   loserPlayer 
           // });
+                     io.to(roomId).emit('gameWon', {
+    winner: winnerPlayer.name,
+    winnerId: winnerPlayer.userId,
+    winningLine,
+    board: room.board
+  });
              // Emit different events for winner and loser
   io.to(winnerPlayer.socketId).emit('winnerScreen', { 
     result: gameResult, 
@@ -502,31 +509,31 @@ function generateRoomId() {
 const checkWin = (board) => {
   const winningLines = [
     // Rows
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
+    [0, 1, 2], [1, 2, 3],
+    [4, 5, 6], [5, 6, 7],
+    [8, 9, 10], [9, 10, 11],
+    [12, 13, 14], [13, 14, 15],
 
     // Columns
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
+    [0, 4, 8], [1, 5, 9], [2, 6, 10], [3, 7, 11],
+    [4, 8, 12], [5, 9, 13], [6, 10, 14], [7, 11, 15],
 
     // Diagonals
-    [0, 5, 10, 15],
-    [3, 6, 9, 12],
+    [0, 5, 10], [1, 6, 11], [2, 7, 12],
+    [3, 6, 9], [4, 9, 14], [5, 10, 15],
+    [6, 9, 12], [7, 10, 13]
   ];
 
   for (let line of winningLines) {
-    const [a, b, c, d] = line;
-    if (board[a] && board[a] === board[b] && board[a] === board[c] && board[a] === board[d]) {
-      return board[a];
+    const [a, b, c] = line;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return { winnerSymbol: board[a], winningLine: line };
     }
   }
 
   return null;
 };
+
 
 
 server.listen(5005, () => console.log("🚀 Server running on port 5005"));
